4daca10363d38a8082b2e85e0b2fca757a387c2c
Yunfan <yfan2012@gmail.com>
2017-09-20 14:34:20 -0400

    first c-tutorial things

---
+++ c-tutorial/hello-world.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+//this is the starting point
+
+/*how do multi-line comments work
+ *do i need these stars
+ *they are annoying
+ */
+//without star or double slash my chars are white. sad. need stars.
+
+
+int main() {
+  printf("hello world! \n");
+}
+
+//it appears double quotes are strings and single quotes are something else
